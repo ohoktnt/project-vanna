e4c199437d901c126442b3870501b9591b3ffa10
Tammy <tammy.n.tran@gmail.com>
2021-03-31 15:54:50 -0400

    info from review with van

--- src/App.js
+++ src/App.js
@@ -4,9 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Vanna Makeup Artistry Website
-      </header>
+      <h1>Vanna Makeup Artistry Website</h1>
+      <ul>Sections
+        <li>About - Quick summary of services, goal, target</li>
+        <li>Contact - social media link, contact info, business hours, direct messaging</li>
+        <li>Services - bundles? pricing? list? wedding decorations and dress rentals, flower arrangements </li>
+        <li>Media - example photos of service, categorized? studio?</li>
+      </ul>
+      <div>
+        ADDITIONAL INFO REQUIRED:
+        1. domain name?
+        2. email ?
+        3. https://www.jenevoymakeuvpstudio.com/
+        4.https://www.canva.com/templates/EAENHtBzdaA-feminine-minimalist-logo-template-with-branch-for-home-decoration/
+        but with brush stokes with VN, nude accents
+        5. Check social media presencse - google instagram page
+      </div>
     </div>
   );
 }
