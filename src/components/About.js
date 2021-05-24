00e43ed66ee7d9f0f8e38b1a845b627949c83c78
Tammy <tammy.n.tran@gmail.com>
2021-05-24 12:56:43 -0400

    set up good for grid gallery

--- src/components/About.js
+++ src/components/About.js
@@ -20,7 +20,7 @@ function About() {
       </InfoContainer>
       <h2>Portfolio</h2>
       <GalleryContainer>
-        <Gallery images={images}/>
+        <Gallery images={images} enableImageSelection={false}/>
       </GalleryContainer>
     </Container>
   );
@@ -52,33 +52,6 @@ const Headshot = styled.img`
 `;
 
 const GalleryContainer = styled.div`
-  border: 1px solid red;
-  display:flex;
-`;
-
-const PortfolioGrid = styled.div`
   display: grid;
-  grid-gap: 5px;
-  grid-template-columns: repeat(4, auto);
 `;
 
-const Wrap = styled.div`
-  cursor: pointer;
-  border-radius: 10px;
-  overflow: hidden; // bc image was overlapping past border
-  border: 3px solid rgba(249, 249, 249, 0.1);
-  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-
-  &:hover {
-    transform: scale(1.05);
-    border-color: rgba(249, 249, 249, 0.8);
-    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
-      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
-  }
-`;
