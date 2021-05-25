6c158f3cf58df7f1459de5f68d6ecd705e8c50f8
Tammy <tammy.n.tran@gmail.com>
2021-05-25 18:30:09 -0400

    redid form style for minimalistic look

--- src/components/Form.js
+++ src/components/Form.js
@@ -56,9 +56,8 @@ function Form() {
   }
 
   return (
-    <form id="contact-form" onSubmit={sendMessage}>
+    <Container id="contact-form" onSubmit={sendMessage}>
       <FormGroup>
-        <label>Name</label>
         <input
           required
           type="text"
@@ -69,9 +68,6 @@ function Form() {
             setFormArgs({ ...formArgs, name: event.target.value })
           }
         />
-      </FormGroup>
-      <FormGroup>
-        <label>Email</label>
         <input
           required
           type="email"
@@ -82,9 +78,6 @@ function Form() {
             setFormArgs({ ...formArgs, email: event.target.value })
           }
         />
-      </FormGroup>
-      <FormGroup>
-        <label>Phone Number</label>
         <input
           required
           type="tel"
@@ -95,9 +88,6 @@ function Form() {
             setFormArgs({ ...formArgs, number: event.target.value })
           }
         />
-      </FormGroup>
-      <FormGroup>
-        <label>Event Date</label>
         <input
           required
           type="date"
@@ -108,9 +98,6 @@ function Form() {
             setFormArgs({ ...formArgs, date: event.target.value });
           }}
         ></input>
-      </FormGroup>
-      <FormGroup>
-        <label>Message</label>
         <textarea
           required
           name="message"
@@ -123,36 +110,44 @@ function Form() {
       </FormGroup>
       <SubmitButton type="submit" value="Send">
         <FontAwesomeIcon icon={faEnvelope} />
-        <span>Send</span>
+        <span> Send</span>
       </SubmitButton>
-    </form>
+    </Container>
   );
 }
 
 export default Form;
 
+const Container = styled.form`
+  padding: 20px;
+`;
+
 const FormGroup = styled.div`
   display: flex;
   flex-direction: column;
-  width: 50%;
+  // width: 50%;
+  align-items: center;
   margin-left: auto;
   margin-right: auto;
 
-  label {
-    text-align: left;
-    padding: 5px;
-  }
 
   input {
-    padding: 5px;
-    margin: 5px 0;
-    border: 0;
-    border-radius: 10px;
-    box-shadow: 0 0 0px 4px rgba(0, 0, 0, 0.06);
+    padding: 0px 15px 0px 15px;
+    width: 470px;
+    height: 50px;
+    border: solid 1px #a6a6a6;
+    border-bottom: none;
 
     &:focus {
-      outline: thick double #808080;
-      outline-radius: 10px;
+      outline: none;
+    }
+
+    &:hover {
+      background: #a6a6a6;
+      color: white;
+      ::placeholder {
+        color: white;
+      }
     }
 
     ::placeholder {
@@ -161,16 +156,28 @@ const FormGroup = styled.div`
   }
 
   textarea {
-    height: 10em;
-    padding: 5px;
-    margin: 5px 0;
-    border: 0;
-    border-radius: 10px;
-    box-shadow: 0 0 0px 4px rgba(0, 0, 0, 0.06);
+    width: 470px;
+    max-width: 470px;
+    height: 110px;
+    max-height: 110px;
+    padding: 15px;
+    // margin: 5px 0;
+
+    border: 1px solid #a6a6a6;
+    outline: none;
+    // border-radius: 10px;
+    // box-shadow: 0 0 0px 4px rgba(0, 0, 0, 0.06);
 
     &:focus {
-      outline: thick double #808080;
-      outline-radius: 10px;
+      outline: none;
+    }
+
+    &:hover {
+      background: #a6a6a6;
+      color: white;
+      ::placeholder {
+        color: white;
+      }
     }
 
     ::placeholder {
@@ -183,26 +190,55 @@ const FormGroup = styled.div`
 `;
 
 const SubmitButton = styled.button`
-  margin: 10px;
-  padding: 12px 14px;
-  border-radius 4px;
-  font-size: 1em;
-  letter-spacing: 1.2px;
+  // margin: 10px;
+  // padding: 12px 14px;
+  // border-radius 4px;
+  // font-size: 1em;
+  // letter-spacing: 1.2px;
+  // cursor: pointer;
+  // background: rgba(0, 0, 0, 0.3);
+  // border: 1px solid rgb(249, 249, 249);
+  // color: rgb(249, 249, 249);
+  // transform-origin: left center;
+  // transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+
+  // &:hover {
+  //   background: rgb(0,0,0)
+  //   // border: 1px solid black;
+  //   // background: white;
+  //   // color: black;
+  // }
+
+  // span {
+  //   margin: 5px;
+  // }
+
+  width: 502px;
+  height: 50px;
+
+  margin: -5px 0px 0px 0px;
+  
+  font-family: 'Lato', sans-serif;
+  font-size: 0.875em;
+  color: #a6a6a6;
+  background: inherit;
+  
+  outline:none;
   cursor: pointer;
-  background: rgba(0, 0, 0, 0.3);
-  border: 1px solid rgb(249, 249, 249);
-  color: rgb(249, 249, 249);
-  transform-origin: left center;
-  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  
+  border: solid 1px #a6a6a6;
+  border-top: none;
+
+  &:focus {
+    outline: none;
+  }
 
   &:hover {
-    background: rgb(0,0,0)
-    // border: 1px solid black;
-    // background: white;
-    // color: black;
+    background: #a6a6a6;
+    color: white;
+    ::placeholder {
+      color: white;
+    }
   }
 
-  span {
-    margin: 5px;
-  }
 `;
