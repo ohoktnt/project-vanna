14c0f5f899a5a9ac7e334c4a5ec1597f188a425e
Tammy <tammy.n.tran@gmail.com>
2021-06-15 14:57:24 -0400

    added new photos to makeup gallery

--- src/components/imagesArr.js
+++ src/components/imagesArr.js
@@ -2,200 +2,221 @@ export const images = {
   null: [],
   makeup: [
     {
+      src: "/images/makeup22.JPG",
+      thumbnail: "/images/makeup22.JPG",
+      thumbnailWidth: 150,
+      thumbnailHeight: 174,
+      alt: "Vanna artistry makeup",
+    },
+    {
+      src: "/images/makeup23.JPG",
+      thumbnail: "/images/makeup23.JPG",
+      thumbnailWidth: 120,
+      thumbnailHeight: 174,
+      alt: "Vanna artistry makeup",
+    },
+    {
+      src: "/images/makeup24.JPG",
+      thumbnail: "/images/makeup24.JPG",
+      thumbnailWidth: 150,
+      thumbnailHeight: 174,
+      alt: "Vanna artistry makeup",
+    },
+    {
       src: "/images/makeup1.JPG",
       thumbnail: "/images/makeup1.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup2.JPG",
       thumbnail: "/images/makeup2.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup3.JPG",
       thumbnail: "/images/makeup3.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup4.JPG",
       thumbnail: "/images/makeup4.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup5.JPG",
       thumbnail: "/images/makeup5.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup6.JPG",
       thumbnail: "/images/makeup6.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup7.JPG",
       thumbnail: "/images/makeup7.JPG",
       thumbnailWidth: 230,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup8.JPG",
       thumbnail: "/images/makeup8.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup9.JPG",
       thumbnail: "/images/makeup9.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup10.JPG",
       thumbnail: "/images/makeup10.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup11.JPG",
       thumbnail: "/images/makeup11.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup12.JPG",
       thumbnail: "/images/makeup12.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup13.JPG",
       thumbnail: "/images/makeup13.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup14.JPG",
       thumbnail: "/images/makeup14.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup15.JPG",
       thumbnail: "/images/makeup15.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup16.JPG",
       thumbnail: "/images/makeup16.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup17.jpg",
       thumbnail: "/images/makeup17.jpg",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup18.JPG",
       thumbnail: "/images/makeup18.JPG",
       thumbnailWidth: 250,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup19.JPG",
       thumbnail: "/images/makeup19.JPG",
       thumbnailWidth: 250,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup20.jpg",
       thumbnail: "/images/makeup20.jpg",
       thumbnailWidth: 250,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/makeup21.jpg",
       thumbnail: "/images/makeup21.jpg",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry makeup"
+      alt: "Vanna artistry makeup",
     },
     {
       src: "/images/hair1.JPG",
       thumbnail: "/images/hair1.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair style"
+      alt: "Vanna artistry hair style",
     },
     {
       src: "/images/hair2.JPG",
       thumbnail: "/images/hair2.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair style"
+      alt: "Vanna artistry hair style",
     },
     {
       src: "/images/hair3.JPG",
       thumbnail: "/images/hair3.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair style"
+      alt: "Vanna artistry hair style",
     },
     {
       src: "/images/hair4.JPG",
       thumbnail: "/images/hair4.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair style"
+      alt: "Vanna artistry hair style",
     },
     {
       src: "/images/hair5.JPG",
       thumbnail: "/images/hair5.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair style"
+      alt: "Vanna artistry hair style",
     },
     {
       src: "/images/hair6.JPG",
       thumbnail: "/images/hair6.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair style"
+      alt: "Vanna artistry hair style",
     },
     {
       src: "/images/hair7.JPG",
       thumbnail: "/images/hair7.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair style"
+      alt: "Vanna artistry hair style",
     },
   ],
   dress: [
@@ -204,91 +225,91 @@ export const images = {
       thumbnail: "/images/dress2.JPG",
       thumbnailWidth: 130,
       thumbnailHeight: 174,
-      alt: "Vanna artistry wedding dress rental"
+      alt: "Vanna artistry wedding dress rental",
     },
     {
       src: "/images/dress3.JPG",
       thumbnail: "/images/dress3.JPG",
       thumbnailWidth: 250,
       thumbnailHeight: 174,
-      alt: "Vanna artistry wedding dress rental"
+      alt: "Vanna artistry wedding dress rental",
     },
     {
       src: "/images/dress4.JPG",
       thumbnail: "/images/dress4.JPG",
       thumbnailWidth: 250,
       thumbnailHeight: 174,
-      alt: "Vanna artistry wedding dress rental"
+      alt: "Vanna artistry wedding dress rental",
     },
     {
       src: "/images/dress5.JPG",
       thumbnail: "/images/dress5.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry wedding dress rental"
+      alt: "Vanna artistry wedding dress rental",
     },
     {
       src: "/images/dress6.JPG",
       thumbnail: "/images/dress6.JPG",
       thumbnailWidth: 220,
       thumbnailHeight: 174,
-      alt: "Vanna artistry wedding dress rental"
+      alt: "Vanna artistry wedding dress rental",
     },
     {
       src: "/images/dress7.JPG",
       thumbnail: "/images/dress7.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry wedding dress rental"
+      alt: "Vanna artistry wedding dress rental",
     },
     {
       src: "/images/dress8.JPG",
       thumbnail: "/images/dress8.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry wedding dress rental"
+      alt: "Vanna artistry wedding dress rental",
     },
     {
       src: "/images/dress9.JPG",
       thumbnail: "/images/dress9.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry wedding dress rental"
+      alt: "Vanna artistry wedding dress rental",
     },
     {
       src: "/images/dress10.JPG",
       thumbnail: "/images/dress10.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry wedding dress rental"
+      alt: "Vanna artistry wedding dress rental",
     },
     {
       src: "/images/dress11.JPG",
       thumbnail: "/images/dress11.JPG",
       thumbnailWidth: 250,
       thumbnailHeight: 174,
-      alt: "Vanna artistry wedding dress rental"
+      alt: "Vanna artistry wedding dress rental",
     },
     {
       src: "/images/dress12.jpg",
       thumbnail: "/images/dress12.jpg",
       thumbnailWidth: 170,
       thumbnailHeight: 174,
-      alt: "Vanna artistry wedding dress rental"
+      alt: "Vanna artistry wedding dress rental",
     },
     {
       src: "/images/dress13.JPG",
       thumbnail: "/images/dress13.JPG",
       thumbnailWidth: 175,
       thumbnailHeight: 174,
-      alt: "Vanna artistry wedding dress rental"
+      alt: "Vanna artistry wedding dress rental",
     },
     {
       src: "/images/dress14.JPG",
       thumbnail: "/images/dress14.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry wedding dress rental"
+      alt: "Vanna artistry wedding dress rental",
     },
   ],
   deco: [
@@ -297,105 +318,105 @@ export const images = {
       thumbnail: "/images/deco1.JPEG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry baby shower event decorations flowers balloons"
+      alt: "Vanna artistry baby shower event decorations flowers balloons",
     },
     {
       src: "/images/deco2.jpg",
       thumbnail: "/images/deco2.jpg",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry decorations flowers"
+      alt: "Vanna artistry decorations flowers",
     },
     {
       src: "/images/deco3.JPG",
       thumbnail: "/images/deco3.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry decorations flowers"
+      alt: "Vanna artistry decorations flowers",
     },
     {
       src: "/images/deco4.jpg",
       thumbnail: "/images/deco4.jpg",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry decorations flowers"
+      alt: "Vanna artistry decorations flowers",
     },
     {
       src: "/images/deco5.JPG",
       thumbnail: "/images/deco5.JPG",
       thumbnailWidth: 250,
       thumbnailHeight: 174,
-      alt: "Vanna artistry decorations flowers backdrop wedding"
+      alt: "Vanna artistry decorations flowers backdrop wedding",
     },
     {
       src: "/images/deco6.JPG",
       thumbnail: "/images/deco6.JPG",
       thumbnailWidth: 250,
       thumbnailHeight: 174,
-      alt: "Vanna artistry decorations flowers backdrop wedding"
+      alt: "Vanna artistry decorations flowers backdrop wedding",
     },
     {
       src: "/images/deco7.JPG",
       thumbnail: "/images/deco7.JPG",
       thumbnailWidth: 130,
       thumbnailHeight: 174,
-      alt: "Vanna artistry decorations flowers backdrop wedding"
+      alt: "Vanna artistry decorations flowers backdrop wedding",
     },
     {
       src: "/images/deco8.jpg",
       thumbnail: "/images/deco8.jpg",
       thumbnailWidth: 130,
       thumbnailHeight: 174,
-      alt: "Vanna artistry decorations flowers backdrop wedding"
+      alt: "Vanna artistry decorations flowers backdrop wedding",
     },
     {
       src: "/images/deco9.JPG",
       thumbnail: "/images/deco9.JPG",
       thumbnailWidth: 130,
       thumbnailHeight: 174,
-      alt: "Vanna artistry decorations flowers backdrop wedding"
+      alt: "Vanna artistry decorations flowers backdrop wedding",
     },
     {
       src: "/images/deco10.JPG",
       thumbnail: "/images/deco10.JPG",
       thumbnailWidth: 250,
       thumbnailHeight: 174,
-      alt: "Vanna artistry decorations flowers backdrop table setting wedding"
+      alt: "Vanna artistry decorations flowers backdrop table setting wedding",
     },
     {
       src: "/images/deco11.jpg",
       thumbnail: "/images/deco11.jpg",
       thumbnailWidth: 130,
       thumbnailHeight: 174,
-      alt: "Vanna artistry decorations flowers backdrop table setting wedding"
+      alt: "Vanna artistry decorations flowers backdrop table setting wedding",
     },
     {
       src: "/images/deco12.jpg",
       thumbnail: "/images/deco12.jpg",
       thumbnailWidth: 250,
       thumbnailHeight: 174,
-      alt: "Vanna artistry decorations flowers backdrop table setting wedding"
+      alt: "Vanna artistry decorations flowers backdrop table setting wedding",
     },
     {
       src: "/images/deco13.JPG",
       thumbnail: "/images/deco13.JPG",
       thumbnailWidth: 130,
       thumbnailHeight: 174,
-      alt: "Vanna artistry decorations flowers backdrop balloons birthday"
+      alt: "Vanna artistry decorations flowers backdrop balloons birthday",
     },
     {
       src: "/images/deco14.JPG",
       thumbnail: "/images/deco14.JPG",
       thumbnailWidth: 130,
       thumbnailHeight: 174,
-      alt: "Vanna artistry decorations flowers backdrop balloons birthday"
+      alt: "Vanna artistry decorations flowers backdrop balloons birthday",
     },
     {
       src: "/images/deco15.jpeg",
       thumbnail: "/images/deco15.jpeg",
       thumbnailWidth: 130,
       thumbnailHeight: 174,
-      alt: "Vanna artistry decorations flowers table decoration wedding"
+      alt: "Vanna artistry decorations flowers table decoration wedding",
     },
     // {
     //   src: "/images/deco16.jpg",
@@ -411,140 +432,140 @@ export const images = {
       thumbnail: "/images/makeup20.jpg",
       thumbnailWidth: 300,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair & makeup model"
+      alt: "Vanna artistry hair & makeup model",
     },
     {
       src: "/images/makeup21.jpg",
       thumbnail: "/images/makeup21.jpg",
       thumbnailWidth: 140,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair & makeup model"
+      alt: "Vanna artistry hair & makeup model",
     },
     {
       src: "/images/artist2.JPG",
       thumbnail: "/images/artist2.JPG",
       thumbnailWidth: 300,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair & makeup model with artist"
+      alt: "Vanna artistry hair & makeup model with artist",
     },
     {
       src: "/images/makeup18.JPG",
       thumbnail: "/images/makeup18.JPG",
       thumbnailWidth: 220,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair & makeup model"
+      alt: "Vanna artistry hair & makeup model",
     },
     {
       src: "/images/makeup19.JPG",
       thumbnail: "/images/makeup19.JPG",
       thumbnailHeight: 174,
       thumbnailWidth: 270,
-      alt: "Vanna artistry hair & makeup model wedding dress"
+      alt: "Vanna artistry hair & makeup model wedding dress",
     },
     {
       src: "/images/artist1.JPG",
       thumbnail: "/images/artist1.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair & makeup model with artist"
+      alt: "Vanna artistry hair & makeup model with artist",
     },
     {
       src: "/images/makeupBA6.jpg",
       thumbnail: "/images/makeupBA6.jpg",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair & makeup model split face"
+      alt: "Vanna artistry hair & makeup model split face",
     },
     {
       src: "/images/makeupBA1.JPEG",
       thumbnail: "/images/makeupBA1.JPEG",
       thumbnailWidth: 300,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair & makeup model before and after"
+      alt: "Vanna artistry hair & makeup model before and after",
     },
     {
       src: "/images/makeupBA2.JPEG",
       thumbnail: "/images/makeupBA2.JPEG",
       thumbnailWidth: 300,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair & makeup model before and after"
+      alt: "Vanna artistry hair & makeup model before and after",
     },
     {
       src: "/images/makeupBA3.JPEG",
       thumbnail: "/images/makeupBA3.JPEG",
       thumbnailWidth: 300,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair & makeup model before and after"
+      alt: "Vanna artistry hair & makeup model before and after",
     },
     {
       src: "/images/makeupBA4.JPEG",
       thumbnail: "/images/makeupBA4.JPEG",
       thumbnailWidth: 300,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair & makeup model before and after"
+      alt: "Vanna artistry hair & makeup model before and after",
     },
     {
       src: "/images/makeupBA5.JPEG",
       thumbnail: "/images/makeupBA5.JPEG",
       thumbnailWidth: 300,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair & makeup model before and after"
+      alt: "Vanna artistry hair & makeup model before and after",
     },
     {
       src: "/images/makeup10.JPG",
       thumbnail: "/images/makeup10.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair & makeup model"
+      alt: "Vanna artistry hair & makeup model",
     },
     {
       src: "/images/hair1.JPG",
       thumbnail: "/images/hair1.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair style model"
+      alt: "Vanna artistry hair style model",
     },
     {
       src: "/images/hair3.JPG",
       thumbnail: "/images/hair3.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair style model"
+      alt: "Vanna artistry hair style model",
     },
     {
       src: "/images/hair4.JPG",
       thumbnail: "/images/hair4.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair style model"
+      alt: "Vanna artistry hair style model",
     },
     {
       src: "/images/hair5.JPG",
       thumbnail: "/images/hair5.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair style model"
+      alt: "Vanna artistry hair style model",
     },
     {
       src: "/images/hair6.JPG",
       thumbnail: "/images/hair6.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair style model"
+      alt: "Vanna artistry hair style model",
     },
     {
       src: "/images/hair7.JPG",
       thumbnail: "/images/hair7.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair style model"
+      alt: "Vanna artistry hair style model",
     },
     {
       src: "/images/pose1.JPG",
       thumbnail: "/images/pose1.JPG",
       thumbnailWidth: 150,
       thumbnailHeight: 174,
-      alt: "Vanna artistry hair & makeup style model"
+      alt: "Vanna artistry hair & makeup style model",
     },
   ],
 };
